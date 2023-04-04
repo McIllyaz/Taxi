02a5289127139ad0694df15d23acb85ff8158f1e
Illyaz Arya <71999096+McIllyaz@users.noreply.github.com>
2023-04-04 21:25:20 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pengenalanclass // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi satu = new Taxi();
+            Taxi dua = new Taxi();
+
+            satu.Name = "Angga";
+            satu.Duty = true;
+            satu.Passanger = 5;
+
+            satu.Detail();
+            satu.Checker();
+
+            dua.Name = "Brilyan";
+            dua.Duty = false;
+            dua.Passanger = 2;
+
+            dua.Detail();
+            dua.Checker();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Pengenalanclass
+{
+	public class Taxi
+	{
+		public string Name { get; set; }
+		public bool Duty { get; set; }
+		public int Passanger { get; set; }
+
+		public void Detail()
+		{
+            Console.WriteLine("Driver name \t\t: {0}", Name);
+            Console.WriteLine("On duty\t\t\t: {0}", Duty);
+            Console.WriteLine("Number of passanger\t: {0}", Passanger);
+        }
+
+		public void Pickup()
+		{
+            Console.WriteLine("{0} sedang menjemput penumpang.\n", Name);
+        }
+
+		public void Dropoff()
+		{
+            Console.WriteLine("{0} selesai mengantar penumpang.\n", Name);
+        }
+
+		public void Checker()
+		{
+            if (Duty == true)
+            {
+                Pickup();
+            }
+            else if (Duty == false)
+            {
+                Dropoff();
+            }
+            else
+            {
+                Console.WriteLine("Driver {0} sedang offline.\n", Name);
+            }
+        }
+        
+
+	}
+}
+
